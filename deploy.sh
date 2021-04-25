d7f6b55d22556253f43ab02e404e2adc47f7de0f
Oliver <ohowe@protonmail.com>
2021-04-24 20:30:10 -0600

    deployer

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+SSH_KEY_PATH="login.pub"
+SERVER="discord@10.0.0.90"
+
+echo "Deploying"
+ssh -i $SSH_KEY_PATH $SERVER 'bash -i'  <<-'ENDSSH'
+    cd ohowebot
+    git pull
+    pm2 stop ohowebot
+    rm -rf node_modules
+    npm install
+    npm run clean
+    npm run build
+    pm2 start ohowebot
+    pm2 save
+    exit
+ENDSSH
+echo "Done"
