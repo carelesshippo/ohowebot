d39bd8bffcd4e588a25defbc7b94545d9d685ec3
Oliver <ohowe@protonmail.com>
2021-05-02 10:57:03 -0600

    deleted ITicket

--- src/ITicket.ts
+++
@@ -1,9 +0,0 @@
-
-export default interface ITicket {
-    id: number;
-    creatorId: string;
-    originalMessageId: string;
-    channelId: string;
-    subject: string;
-    resolved: boolean;
-}
